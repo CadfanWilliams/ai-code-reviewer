ef6e5db1eea76ae905f9677320e30fc0b06e2809
John Williams <cadfanw@gmail.com>
2026-03-15 19:24:51 +0000

    added pullrequestevent record

--- src/main/java/ai_log_reviewer/Model/PullRequestEvent.java
+++ src/main/java/ai_log_reviewer/Model/PullRequestEvent.java
@@ -1,10 +1,26 @@
 package ai_log_reviewer.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PullRequestEvent {
-    //make model for improtant stuff from json data
-    //diff url for AI llm to review
-    //title
-    //user
-    //
+public record PullRequestEvent(
+        String action,
+
+        @JsonProperty("pull_request")
+        PullRequest pullRequest,
+
+        Repository repository
+) {
+    public record PullRequest(
+            String number,
+            String title,
+            String body,
+
+            @JsonProperty("diff_url")
+            String diffUrl
+    ) {}
+
+    public record Repository(
+            @JsonProperty("full_name")
+            String fullName
+    ) {}
 }
